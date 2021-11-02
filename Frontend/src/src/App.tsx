e03280cb12f95d451ea450b48e0cb30f7f763bce
codeolam <frankuchenna30@gmail.com>
2021-11-02 18:10:53 +0100

    added frontend app

---
+++ Frontend/src/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-form">
+        <p className=".App-header">Upload PDF</p>
+        <form action="/action_page.php">
+          <input type="file" id="myFile" name="filename" />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
